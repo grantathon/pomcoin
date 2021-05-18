e07b43768dd65a1463c93e86a4c1ddbf77eca865
grantathon <grant.bartel@gmail.com>
2021-05-18 11:58:59 +0300

    Able to deploy POM with intially minted supply

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,12 @@ var Pomcoin = artifacts.require("Pomcoin.sol");
 module.exports = async function(deployer) {
   await deployer.deploy(Pomcoin);
 
-  // TODO: Mint initial supply
+  let instancePomcoin = await Pomcoin.deployed();
 
+  // TODO: Mint initial supply
+  await instancePomcoin.mint(7000000000e6);
 
   // TODO: Permanently stake initial liquidity to DEX
 
-  
+
 };
