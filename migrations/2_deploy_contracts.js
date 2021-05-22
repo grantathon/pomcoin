a46c6bde2a540bb517cfa2956a98a862a0d2a1fa
grantathon <grant.bartel@gmail.com>
2021-05-22 11:45:23 +0300

    Working on adding LP to Pancakeswap

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,14 @@
 var Pomcoin = artifacts.require("Pomcoin.sol");
 var EmissionScheduler = artifacts.require("EmissionScheduler.sol");
 
-// PancakeSwap mainnet
-// Router v1: 0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F
-// Factory v1: 0xBCfCcbde45cE874adCB698cC183deBcF17952812
-//
-// Router v2: 0x10ED43C718714eb63d5aA57B78B54704E256024E
-// Factory v2: 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73
+const pancakeswapFactoryAbi = require("../contracts/abi/PancakeFactoryV2.json");
+const pancakeswapRouterAbi = require("../contracts/abi/PancakeRouterV2.json");
 
-// PancakeSwap testnet
-// Factory: 0x6725F303b657a9451d8BA641348b6761A6CC7a17
-// Router: 0xD99D1c33F9fC3444f8101754aBC46c52416550D1
+PANCAKESWAP_FACTORY_ADDR = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";  // Mainnet
+PANCAKESWAP_ROUTER_ADDR = "0x10ED43C718714eb63d5aA57B78B54704E256024E";  // Mainnet
+// PANCAKESWAP_FACTORY_ADDR = "0x6725F303b657a9451d8BA641348b6761A6CC7a17";  // Testnet
+// PANCAKESWAP_ROUTER_ADDR = "0xD99D1c33F9fC3444f8101754aBC46c52416550D1";  // Testnet
+WBNB_ADDR = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 
 module.exports = async function(deployer) {
   await deployer.deploy(Pomcoin, 350000000);
@@ -23,29 +21,27 @@ module.exports = async function(deployer) {
   await web3.eth.sendTransaction({
     from: creator,
     to: Pomcoin.address,
-    value: web3.utils.toWei("1", "ether")
+    value: web3.utils.toWei("2", "ether")
   });
 
   // TODO: Permanently stake initial liquidity to DEX
-  const factory = await Factory.at('0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73');
-  const router = await Router.at('0x10ED43C718714eb63d5aA57B78B54704E256024E');
-  const token1 = await Token1.new();
-  const token2 = await Token2.new();
-  const pairAddress = await factory.createPair.call(token1.address, token2.address);
-  const tx = await factory.createPair(token1.address, token2.address);
-  await token1.approve(router.address, 10000);
-  await token2.approve(router.address, 10000);
-  await router.addLiquidity(  
-    token1.address,
-    token2.address,
-    10000,
-    10000,
-    10000,
-    10000,
-    creator,
+  var factory = await new web3.eth.Contract(pancakeswapFactoryAbi, PANCAKESWAP_FACTORY_ADDR);
+  var router = await new web3.eth.Contract(pancakeswapRouterAbi, PANCAKESWAP_ROUTER_ADDR);
+  const weth = await router.methods.WETH().call();
+  const pair = await factory.methods.createPair(Pomcoin.address, weth).send({
+    from: creator,
+    gas: 5000000
+  });
+  await instancePomcoin.approve(PANCAKESWAP_ROUTER_ADDR, 350000000);
+  await router.methods.addLiquidityETH(
+    Pomcoin.address,
+    350000000,
+    350000000,
+    web3.utils.toWei("1", "ether"),
+    creator,  // TODO: Change to Pomcoin address
     Math.floor(Date.now() / 1000) + 60 * 10
-  );
-  const pair = await Pair.at(pairAddress);
-  const balance = await pair.balanceOf(creator);
-
+  ).send({
+    from: creator,
+    gas: 5000000
+  });
 };
--- truffle-config.js
+++ truffle-config.js
@@ -38,6 +38,12 @@ module.exports = {
   compilers: {
     solc: {
       version: "0.6.12",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
     },
+  },
   },
 };
