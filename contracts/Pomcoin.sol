3e816f9ab0cb4cec8a0867d37ef4b2414c21de49
grantathon <grant.bartel@gmail.com>
2021-05-28 10:56:26 +0300

    Migrated LP launch from deploy script to constructor

--- contracts/Pomcoin.sol
+++ contracts/Pomcoin.sol
@@ -2,18 +2,47 @@
 pragma solidity 0.6.12;
 
 import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
+import "@pancakeswap-libs/pancake-swap-core/contracts/interfaces/IPancakeFactory.sol";
 import "pancakeswap-peripheral/contracts/interfaces/IPancakeRouter02.sol";
+import "pancakeswap-peripheral/contracts/interfaces/IWETH.sol";
 
 contract Pomcoin is BEP20 {
-  IPancakeRouter02 pancakeRouterV2;
+  IPancakeFactory public pancakeFactoryV2;
+  IPancakeRouter02 public pancakeRouterV2;
+
+  address pancakeFactoryV2Addr = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
+  address pancakeRouterV2Addr = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
 
   event SwapAndLiquify(uint tokensIntoLiqudity, uint ethIntoLiquidity);
 
   constructor(uint initialSupply) BEP20("Pomcoin", "POM") public {
-    pancakeRouterV2 = IPancakeRouter02(0x10ED43C718714eb63d5aA57B78B54704E256024E);  // mainnet
-    /* pancakeRouterV2 = IPancakeRouter02(0xD99D1c33F9fC3444f8101754aBC46c52416550D1);  // testnet */
+    pancakeFactoryV2 = IPancakeFactory(pancakeFactoryV2Addr);
+    pancakeRouterV2 = IPancakeRouter02(pancakeRouterV2Addr);
 
-    _mint(msg.sender, initialSupply.mul(10 ** uint(decimals())));
+    // Mint initial supply to the contract
+    _mint(address(this), initialSupply.mul(10 ** uint(decimals())));
+
+    // Fund the contract with ETH from the creator
+    address weth = pancakeRouterV2.WETH();
+    uint ethForLiquidityPool = 1 ether;
+    IWETH(weth).deposit{value: ethForLiquidityPool}();
+    IWETH(weth).transfer(address(this), ethForLiquidityPool);
+
+    // Create liquidity pool pair and permanently stake initial liquidity
+    pancakeFactoryV2.createPair(address(this), weth);
+    uint pomForLiquidityPool = balanceOf(address(this));
+    approve(pancakeRouterV2Addr, pomForLiquidityPool);
+    pancakeRouterV2.addLiquidityETH{value: ethForLiquidityPool}(
+      address(this),
+      pomForLiquidityPool,
+      pomForLiquidityPool,
+      ethForLiquidityPool,
+      address(this),  // Pomcoin owns the LP token, not the creator
+      block.timestamp
+    );
+
+    /* TODO: Burn the LP token (0x000000000000000000000000000000000000dEaD) */
+
   }
 
   receive() external payable {
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
 var Pomcoin = artifacts.require("Pomcoin.sol");
 var EmissionScheduler = artifacts.require("EmissionScheduler.sol");
 
-const pancakeswapFactoryAbi = require("../contracts/abi/PancakeFactoryV2.json");
-const pancakeswapRouterAbi = require("../contracts/abi/PancakeRouterV2.json");
+// const pancakeswapFactoryAbi = require("../contracts/abi/PancakeFactoryV2.json");
+// const pancakeswapRouterAbi = require("../contracts/abi/PancakeRouterV2.json");
 
 require("dotenv").config({path: "../.env"});
 
-PANCAKESWAP_FACTORY_ADDR = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";  // Mainnet
-PANCAKESWAP_ROUTER_ADDR = "0x10ED43C718714eb63d5aA57B78B54704E256024E";  // Mainnet
+// PANCAKESWAP_FACTORY_ADDR = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";  // Mainnet
+// PANCAKESWAP_ROUTER_ADDR = "0x10ED43C718714eb63d5aA57B78B54704E256024E";  // Mainnet
 // PANCAKESWAP_FACTORY_ADDR = "0x6725F303b657a9451d8BA641348b6761A6CC7a17";  // Testnet
 // PANCAKESWAP_ROUTER_ADDR = "0xD99D1c33F9fC3444f8101754aBC46c52416550D1";  // Testnet
 
@@ -15,34 +15,34 @@ module.exports = async function(deployer) {
   await deployer.deploy(Pomcoin, process.env.INITIAL_TOKENS)
   await deployer.deploy(EmissionScheduler);
 
-  const instancePomcoin = await Pomcoin.deployed();
-  const creator = await instancePomcoin.getOwner();
-
-  // Create liquidity pool pair
-  var factory = await new web3.eth.Contract(pancakeswapFactoryAbi, PANCAKESWAP_FACTORY_ADDR);
-  var router = await new web3.eth.Contract(pancakeswapRouterAbi, PANCAKESWAP_ROUTER_ADDR);
-  const weth = await router.methods.WETH().call();
-  const pair = await factory.methods.createPair(Pomcoin.address, weth).send({
-    from: creator,
-    gas: 5000000
-  });
-
-  // Permanently stake initial liquidity
-  var creatorPomBalance = await instancePomcoin.balanceOf(creator);
-  await instancePomcoin.approve(PANCAKESWAP_ROUTER_ADDR, creatorPomBalance);
-  const ethForLiquidityPool = await web3.utils.toWei("1", "ether");
-  await router.methods.addLiquidityETH(
-    Pomcoin.address,
-    creatorPomBalance,
-    creatorPomBalance,
-    ethForLiquidityPool,
-    Pomcoin.address,  // Pomcoin owns the LP token, not creator
-    Math.floor(Date.now() / 1000) + 60 * 10
-  ).send({
-    from: creator,
-    gas: 5000000,
-    value: ethForLiquidityPool
-  });
+  // const instancePomcoin = await Pomcoin.deployed();
+  // const creator = await instancePomcoin.getOwner();
+  //
+  // // Create liquidity pool pair
+  // var factory = await new web3.eth.Contract(pancakeswapFactoryAbi, PANCAKESWAP_FACTORY_ADDR);
+  // var router = await new web3.eth.Contract(pancakeswapRouterAbi, PANCAKESWAP_ROUTER_ADDR);
+  // const weth = await router.methods.WETH().call();
+  // const pair = await factory.methods.createPair(Pomcoin.address, weth).send({
+  //   from: creator,
+  //   gas: 5000000
+  // });
+  //
+  // // Permanently stake initial liquidity
+  // var creatorPomBalance = await instancePomcoin.balanceOf(creator);
+  // await instancePomcoin.approve(PANCAKESWAP_ROUTER_ADDR, creatorPomBalance);
+  // const ethForLiquidityPool = await web3.utils.toWei("1", "ether");
+  // await router.methods.addLiquidityETH(
+  //   Pomcoin.address,
+  //   creatorPomBalance,
+  //   creatorPomBalance,
+  //   ethForLiquidityPool,
+  //   Pomcoin.address,  // Pomcoin owns the LP token, not creator
+  //   Math.floor(Date.now() / 1000) + 60 * 10
+  // ).send({
+  //   from: creator,
+  //   gas: 5000000,
+  //   value: ethForLiquidityPool
+  // });
 
   // TODO: Burn the LP token (0x000000000000000000000000000000000000dEaD)
 
